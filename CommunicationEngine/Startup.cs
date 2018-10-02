09bdcdda9fc75b5d34d7906c832a50ded900b79e
Chashika Weerathunga <ChashikaW@99x.lk>
2018-10-02 15:54:22 +0530

    add files

---
+++ CommunicationEngine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunicationEngine.Startup))]
+namespace CommunicationEngine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
